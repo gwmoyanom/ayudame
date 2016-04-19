1a87889e704a7aeeee54d56365bd2b790dc56328
Andrés Villenas <andresvillenas@hotmail.com>
2016-04-18 22:39:39 -0500

    A little structure of the project. Pending to have the entities.

---
+++ Oshyn.Ayudame.Core/IRequestRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Oshyn.Ayudame.Core
+{
+    internal interface IRequestRepository
+    {
+        public Request GetById(int id)
+        {
+
+        }
+
+        public IEnumerable<Request> GetByMobileUser(string mobileUserId)
+        {
+
+        }
+    }
+}
---
+++ Oshyn.Ayudame.Core/RequestManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oshyn.Ayudame.Core
+{
+    public class RequestManager
+    {
+        private IRequestRepository requestRepository;
+
+        public RequestManager(IRequestRepository requestRepository)
+        {
+            this.requestRepository = requestRepository;
+        }
+
+
+
+
+
+    }
+}
